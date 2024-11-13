1d2cda822fda343ca8375e689010fa56ad092133
AlexandroHenry <alaxhenry0121@gmail.com>
2024-11-13 12:20:06 +0900

    get test

---
+++ myblog/src/main/kotlin/com/alaxhenry/myblog/controller/BankController.kt
@@ -0,0 +1,23 @@
+package com.alaxhenry.myblog.controller
+
+import com.alaxhenry.myblog.model.Bank
+import com.alaxhenry.myblog.service.BankService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/banks")
+class BankController(private val service: BankService) {
+
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
+        ResponseEntity(e.message, HttpStatus.NOT_FOUND)
+
+
+    @GetMapping
+    fun getBanks(): Collection<Bank> = service.getBanks()
+
+    @GetMapping("/{accountNumber}")
+    fun getBank(@PathVariable accountNumber: String) = service.getBank(accountNumber)
+}
--- myblog/src/main/kotlin/com/alaxhenry/myblog/datasource/BankDataSource.kt
+++ myblog/src/main/kotlin/com/alaxhenry/myblog/datasource/BankDataSource.kt
@@ -4,4 +4,5 @@ import com.alaxhenry.myblog.model.Bank
 
 interface BankDataSource {
     fun retrieveBanks(): Collection<Bank>
+    fun retrieveBank(accountNumber: String): Bank
 }
